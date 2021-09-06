44bfcc9755b2e1ff26717ebacc14430d12fe8baf
vantablanta <njerimwanjiku@gmail.com>
2021-09-06 13:32:35 +0300

    pyhton refresher

---
+++ main.py
@@ -0,0 +1,10 @@
+print('Hello World')
+age = 20
+name = "Michelle"
+""
+print("My name is " + name + "."  "I am turning " + str(age))
+
+name2, age2 = "I am Michelle", 20
+print(name2)
+print(age2)
+
