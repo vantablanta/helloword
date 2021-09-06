6c1d68cce15ecdaf13d235d4c09474e3ebda1827
vantablanta <njerimwanjiku@gmail.com>
2021-09-06 13:47:13 +0300

    refresher

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ name2, age2 = "I am Michelle", 20
 print(name2)
 print(age2)
 
+x = 2
+y = 10
+print(y - x)
+print(x * y)
+print(y % x)
+print(y / x)
+
+sent1 = "Today is a good day. "
+print(sent1 * 2)
+sent2 = "I will go fishing"
+print(sent1 + sent2)
+
+sent = "Michelle is playing  cricket"
+print(sent[0:9])
+
+
+
+
